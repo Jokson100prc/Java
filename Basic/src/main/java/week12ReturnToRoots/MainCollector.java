bea8e3d692c0032907e3502350f610189dfe8cda
Jokson100prc <jok.stylista@gmail.com>
2019-03-14 21:04:44 +0100

    simply array, nothing more.... realy? ];->

--- Basic/src/main/java/week12ReturnToRoots/MainCollector.java
+++ Basic/src/main/java/week12ReturnToRoots/MainCollector.java
@@ -4,7 +4,8 @@ public class MainCollector {
   public static void main(String[] args) {
     CollectionExercises test = new CollectionExercises();
     int[] intNumbers = new int[10];
-    int[] duplicate = {11,22,33,44,55,66,77,88,99,100};
+    int[] duplicate = new int[10];
     test.add10numbers(intNumbers, duplicate);
+    test.showSize(intNumbers);
   }
 }
