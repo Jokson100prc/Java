6f7cde95bd4920da2be80a5e323f22c3ed6ff43e
Jokson100prc <jok.stylista@gmail.com>
2019-03-15 20:05:44 +0100

    prarams, args and return - wasn't needed

--- Basic/src/main/java/week12ReturnToRoots/CollectionExercises.java
+++ Basic/src/main/java/week12ReturnToRoots/CollectionExercises.java
@@ -6,18 +6,90 @@ public class CollectionExercises {
   int[] duplicate = {11, 22, 33, 44, 55, 66, 77, 88, 99, 100};
   int number;
 
-  void add10numbers(int[] intNumbers, int[] duplicate) {
+  void add10numbers() {
+
     for (int i = 0; i < 10; i++) {
+
       number++;
+
       this.intNumbers[i] = number;
       System.out.println(this.intNumbers[i]);
+
       this.intNumbers[i] = this.duplicate[i];
       System.out.println(this.intNumbers[i]);
+
     }
   }
 
-  void showSize(int[] intNumbers) {
+  void showSize() {
     System.out.println("Array length is: " + intNumbers.length);
   }
 
+  void multiplicationOfArrayElements() {
+    add10numbers();
+    for (int i = 0; i < intNumbers.length; i++) {
+      intNumbers[i] *= 2;
+      System.out.println(intNumbers[i]);
+      duplicate[i] *= duplicate[i];
+      System.out.println(duplicate[i]);
+    }
+  }
+
 }
+
+
+// args and prarams - wasn't needed here
+// int[] return - wasn't needed too
+
+//  int[] intNumbers = new int[10];
+//  int[] duplicate = {11, 22, 33, 44, 55, 66, 77, 88, 99, 100};
+//  int number;
+//
+//  int[] add10numbers(int[] intNumbers, int[] duplicate) {
+//
+//    for (int i = 0; i < 10; i++) {
+//
+//      number++;
+//
+//      this.intNumbers[i] = number;
+//      System.out.println(this.intNumbers[i]);
+//
+//      this.intNumbers[i] = this.duplicate[i];
+//      System.out.println(this.intNumbers[i]);
+//    }
+//    return this.duplicate;
+//  }
+//
+//  void showSize(int[] intNumbers) {
+//    System.out.println("Array length is: " + intNumbers.length);
+//  }
+//
+//  void multiplicationOfArrayElements() {
+//    add10numbers(intNumbers, duplicate);
+//    for (int i = 0; i < intNumbers.length; i++) {
+//      intNumbers[i] *= 2;
+//      System.out.println(intNumbers[i]);
+//      duplicate[i] *= duplicate[i];
+//      System.out.println(duplicate[i]);
+//    }
+//  }
+
+
+
+//  int[] intNumbers = new int[10];
+//  int[] duplicate = {11, 22, 33, 44, 55, 66, 77, 88, 99, 100};
+//  int number;
+//
+//  void add10numbers(int[] intNumbers, int[] duplicate) {
+//    for (int i = 0; i < 10; i++) {
+//      number++;
+//      this.intNumbers[i] = number;
+//      System.out.println(this.intNumbers[i]);
+//      this.intNumbers[i] = this.duplicate[i];
+//      System.out.println(this.intNumbers[i]);
+//    }
+//  }
+//
+//  void showSize(int[] intNumbers) {
+//    System.out.println("Array length is: " + intNumbers.length);
+//  }
--- Basic/src/main/java/week12ReturnToRoots/MainCollector.java
+++ Basic/src/main/java/week12ReturnToRoots/MainCollector.java
@@ -1,11 +1,33 @@
 package week12ReturnToRoots;
 
 public class MainCollector {
+
   public static void main(String[] args) {
+
     CollectionExercises test = new CollectionExercises();
-    int[] intNumbers = new int[10];
-    int[] duplicate = new int[10];
-    test.add10numbers(intNumbers, duplicate);
-    test.showSize(intNumbers);
+
+    test.add10numbers();
+
+    test.showSize();
+
+    test.multiplicationOfArrayElements();
   }
 }
+
+// args and prarams wasn't needed here
+
+//public class MainCollector {
+//
+//  public static void main(String[] args) {
+//
+//    CollectionExercises test = new CollectionExercises();
+//
+//    int[] intNumbers = new int[10];
+//    int[] duplicate = new int[10];
+//
+//    test.add10numbers(intNumbers, duplicate);
+//    test.showSize(intNumbers);
+//
+//    test.multiplicationOfArrayElements();
+//  }
+//}
